08c67416750565283e6ba33a47ccb3e8ed6c7b15
ishankaler <kumarkalerishan@gmail.com>
2022-07-08 11:17:40 +0530

    fix code compilation bug

--- judge/views.py
+++ judge/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Problem, Submission, TestCase
 from .submissionForm import codeForm
-import os, subprocess
+import os, subprocess, sys
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -31,8 +31,8 @@ def checker(output, correct_ouput):
         print("first")
         return False
     for i in range(len(output)):
-        if list(filter(None, list(output[i].split(' ')))) != \
-            list(filter(None, list(correct_ouput[i].split(' ')))):
+        if list(filter(None, output[i].split(' '))) != \
+            list(filter(None, correct_ouput[i].split(' '))):
             print("second")
             print(list(output[i].split(' ')))
             print(list(correct_ouput[i].split(' ')))
@@ -40,39 +40,41 @@ def checker(output, correct_ouput):
     return True
 
 def evaluate(submission):
-    with open("sol.cpp", "w") as f:
-        f.write(submission.code)
-    cmd =['g++','sol.cpp']
-    test_case = get_object_or_404(TestCase, problem = submission.problem)
-    f_input = test_case.input
-    print(input)
-    res = ""
-    try:
-        res = subprocess.run(cmd, capture_output = True, shell = True, check = True)
-    except subprocess.CalledProcessError:
-        print("CE")
-        # print(subprocess.CalledProcessError.output)
-        # print(res.stdout)
-        # print(res.stderr)
-        submission.verdict = "CE"
-        submission.save()
-        return
-    submission.verdict = "Compiled"
-    submission.save()
-    cmd = ['a.exe']
-    try:
-        output = subprocess.run(cmd, capture_output = True, shell =True, \
-                text = True, input = f_input, check = True, timeout = 2)
-    except subprocess.TimeoutExpired:
-        submission.verdict = "TLE"
-        submission.save()
-        return
-    if checker(output.stdout, test_case.output):
-        submission.verdict = "AC"
-        submission.save()
-    else:
-        submission.verdict = "WA"
-        submission.save()
+	with open("sol.cpp", "w") as f:
+		f.write(submission.code)
+	cmd =['g++ sol.cpp']
+	test_case = get_object_or_404(TestCase, problem = submission.problem)
+	f_input = test_case.input
+	print(input)
+	res = ""
+	try:
+		res = subprocess.run(cmd, capture_output = True, shell = True, check = True, timeout=3)
+	except subprocess.CalledProcessError:
+		print("CE")
+		submission.verdict = "CE"
+		submission.save()
+		return
+	submission.verdict = "Compiled"
+	submission.save()
+	if sys.platform == 'linux':
+		cmd = ['./a.out']
+	else:
+		cmd = ['a.exe']
+	try:
+		output = subprocess.run(cmd, capture_output = True, shell =True, \
+				text = True, input = f_input, check = True, timeout = 2)
+	except subprocess.TimeoutExpired:
+		submission.verdict = "TLE"
+		submission.save()
+		return
+	if checker(output.stdout, test_case.output):
+		submission.verdict = "AC"
+		submission.save()
+	else:
+		submission.verdict = "WA"
+		submission.save()
+
+
 def submit(request, prob_id):
     obj = get_object_or_404(Problem, id = prob_id)
     if request.method == 'POST':
--- sol.cpp
+++ sol.cpp
@@ -1,16 +1,16 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-int main(){
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (auto& e:a){
-        cin >> e;
-    }
-    sort(a.begin(), a.end());
-    for (int ele:a){
-        cout << ele << " ";
-    }
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& e:a){
+        cin >> e;
+    }
+    sort(a.rbegin(), a.rend());
+    for (int ele:a){
+        cout << ele << " ";
+    }
 }
