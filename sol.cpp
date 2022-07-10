bec51d862e182c161fe6a98389bdb49e1993d461
ishankumarkaler <kumarkalerishan@gmail.com>
2022-07-10 11:29:23 +0530

    UI and routing changes

--- sol.cpp
+++ sol.cpp
@@ -1,16 +1,1 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-
-int main(){
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (auto& e:a){
-        cin >> e;
-    }
-    sort(a.begin(), a.end());
-    for (int ele:a){
-        cout << ele << " ";
-    }
-}
+asdf
