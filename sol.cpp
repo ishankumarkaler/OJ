1d704f5096395e0eaec064b7ff4b28e5ed98c341
ishankaler <kumarkalerishan@gmail.com>
2022-07-23 20:10:38 +0530

    update server timezone, test-cases

--- sol.cpp
+++ sol.cpp
@@ -3,23 +3,5 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (auto& e:a){
-        cin >> e;
-    }
-    for (auto it = a.begin(); it != a.end(); it++)
-    {
-        // Searching the upper bound, i.e., first
-        // element greater than *it from beginning
-        auto const insertion_point =
-                std::upper_bound(a.begin(), it, *it);
-
-        // Shifting the unsorted part
-        std::rotate(insertion_point, it, it+1);
-    }
-    for (int ele:a){
-        cout << ele << " ";
-    }
+    cout << "Hello World!";
 }
